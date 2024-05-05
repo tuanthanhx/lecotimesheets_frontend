9d55c7d0b17c9cc1908af28b15ff8d6986fafd01
FASTCODING Thanh <ngo.thanh@fastcoding.jp>
2024-05-05 22:41:01 +0700

    [upd]

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import { setupLayouts } from 'virtual:generated-layouts';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory('/timesheets'),
+  // base: '/timesheets',
   extendRoutes: setupLayouts,
 });
 
